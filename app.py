98b76ee8012c33b220f1910a3c714fd149994c86
Aakash Kuragayala <st125050@ait.ac.th>
2025-02-12 11:50:25 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,16 @@
 import streamlit as st
 import torch
-from transformers import AutoTokenizer
+from transformers import AutoTokenizer, AutoModel
 from sklearn.metrics.pairwise import cosine_similarity
-from bert_model import BERT, mean_pool
+from model.bert_model import mean_pool
 
 # Set device for computation (GPU if available, otherwise CPU)
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-# Load the trained model and tokenizer
-model_path = 'bert.pt'
-params, state = torch.load(model_path, map_location=device)
-model = BERT(**params, device=device).to(device)
-model.load_state_dict(state)
-tokenizer = AutoTokenizer.from_pretrained('bert-base-uncased')
+# Load the pre-trained model and tokenizer from Hugging Face
+model_name = 'sentence-transformers/all-MiniLM-L6-v2'
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+model = AutoModel.from_pretrained(model_name).to(device)
 
 def calculate_similarity(model, tokenizer, sentence_a, sentence_b, device):
     inputs_a = tokenizer(sentence_a, return_tensors='pt', max_length=128, truncation=True, padding='max_length').to(device)
@@ -21,12 +19,12 @@ def calculate_similarity(model, tokenizer, sentence_a, sentence_b, device):
     attention_a = inputs_a['attention_mask']
     inputs_ids_b = inputs_b['input_ids']
     attention_b = inputs_b['attention_mask']
-    segment_ids = torch.zeros(1, 128, dtype=torch.int32).to(device)
 
-    u = model.get_last_hidden_state(inputs_ids_a, segment_ids)
-    v = model.get_last_hidden_state(inputs_ids_b, segment_ids)
-    u = mean_pool(u, attention_a).detach().cpu().numpy().reshape(-1)
-    v = mean_pool(v, attention_b).detach().cpu().numpy().reshape(-1)
+    with torch.no_grad():
+        u = model(**inputs_a).last_hidden_state
+        v = model(**inputs_b).last_hidden_state
+    u = mean_pool(u, attention_a).cpu().numpy().reshape(-1)
+    v = mean_pool(v, attention_b).cpu().numpy().reshape(-1)
     similarity_score = cosine_similarity(u.reshape(1, -1), v.reshape(1, -1))[0, 0]
     return similarity_score
 
